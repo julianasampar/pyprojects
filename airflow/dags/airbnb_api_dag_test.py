2ee091491920e2e100bf22a85802e1f12f10b01f
Juliana Sampar <juliana.s.sampar@gmail.com>
2025-01-23 19:53:16 -0300

    add cred modularization and script file

--- airflow/dags/airbnb_api_dag_test.py
+++ airflow/dags/airbnb_api_dag_test.py
@@ -1,31 +1,45 @@
 ## Importing libraries from Airflow
 
+import sys
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from datetime import datetime
+
+## Importing local scripts
+
+sys.path.append("/home/tabas/personal-dev/pyprojects")
+from pipelines.utils.get_credentials import getting_gcp_credentials
+
+## Creating DAG
 
 with DAG(
     "airbnb_api_dag_test"
     , start_date=datetime(2025, 1, 20)
     , schedule_interval='@weekly'
     , catchup=False
-    , tag=['airbnb_api', 'api']
+    , tags=['airbnb_api', 'api']
 ):
+    
+    ## Defining DAG tasks
     
     get_credentials = PythonOperator(
         task_id = 'get_credentials'
+        , python_callable=getting_gcp_credentials
     )
     
     get_api_parameters = PythonOperator(
         task_id = 'get_api_parameters'
+        , python_callable=getting_gcp_credentials
     )
     
     get_api_request = PythonOperator(
         task_id = 'get_api_request'
+        , python_callable=getting_gcp_credentials
     )
     
     unload_data_to_gcp = PythonOperator(
         task_id = 'unload_data_to_gcp'
+        , python_callable=getting_gcp_credentials
     )
     
     get_credentials >> get_api_parameters >> get_api_request >> unload_data_to_gcp
---
+++ pipelines/airbnb_api_v0/airbnb_api_script.py
@@ -0,0 +1,2 @@
+## This script define the functions to be called on Airbnb API DAG
+
