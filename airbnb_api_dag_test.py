134be82292a336f5abe8b1bd13839eca3e09e7e9
Juliana Sampar <juliana.s.sampar@gmail.com>
2025-01-23 19:10:42 -0300

    create dag file v0

---
+++ airbnb_api_dag_test.py
@@ -0,0 +1,32 @@
+## Importing libraries from Airflow
+
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime
+
+with DAG(
+    "airbnb_api_dag_test"
+    , start_date=datetime(2025, 1, 20)
+    , schedule_interval='@weekly'
+    , chatchup=False
+    , tag=['airbnb_api', 'api']
+):
+    
+    get_credentials = PythonOperator(
+        task_id = 'get_credentials'
+    )
+    
+    get_api_parameters = PythonOperator(
+        task_id = 'get_api_parameters'
+    )
+    
+    get_api_request = PythonOperator(
+        task_id = 'get_api_request'
+    )
+    
+    unload_data_to_gcp = PythonOperator(
+        task_id = 'unload_data_to_gcp'
+    )
+    
+    get_credentials >> get_api_parameters >> get_api_request >> unload_data_to_gcp
+
