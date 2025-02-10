a96758a32d244a4c30a643a5621a0cc0dfc37f15
Juliana Sampar <juliana.s.sampar@gmail.com>
2025-02-09 23:26:33 -0300

    fix bug

--- airflow/dags/spotify_api_dag.py
+++ airflow/dags/spotify_api_dag.py
@@ -14,7 +14,7 @@ import pipelines.utils.common as com
 with DAG(
     "extract_spotify_api_data"
     , start_date=datetime(2025, 2, 1)
-    , schedule_interval=datetime.timedelta(days=15)
+    , schedule_interval=timedelta(days=15)
     , catchup=False
     , tags=['spotify_api', 'api']
 ):
