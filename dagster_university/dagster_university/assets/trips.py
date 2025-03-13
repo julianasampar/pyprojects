22650ebc77ca6ecf44736e3ed4c25e6424335c00
julianasampar <juliana.s.sampar@gmail.com>
2025-03-13 17:44:51 -0300

    Add new asset and declare constants directly on the folder to avoid bug

--- dagster_university/dagster_university/assets/trips.py
+++ dagster_university/dagster_university/assets/trips.py
@@ -2,8 +2,9 @@ import requests
 import sys
 import dagster as dg
 
-sys.path.append("dagster_university/dagster_university/assets")
-import constants
+TAXI_TRIPS_TEMPLATE_FILE_PATH = "data/raw/taxi_trips_{}.parquet"
+TAXI_ZONES_FILE_PATH = "data/raw/taxi_zones.csv"
+
 
 @dg.asset
 def taxi_trips_file():
@@ -15,5 +16,18 @@ def taxi_trips_file():
         f"https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{month_to_fetch}.parquet"
     )
 
-    with open(constants.TAXI_TRIPS_TEMPLATE_FILE_PATH.format(month_to_fetch), "wb") as output_file:
+    with open(TAXI_TRIPS_TEMPLATE_FILE_PATH.format(month_to_fetch), "wb") as output_file:
         output_file.write(raw_trips.content)
+
+@dg.asset
+def taxi_zone_file():
+    """
+      The raw parquet files for the taxi zone dataset. Sourced from the NYC Open Data portal.
+      The data contains distinct keys and values for each zone of NYC.
+    """
+    raw_zones = requests.get(
+        "https://community-engineering-artifacts.s3.us-west-2.amazonaws.com/dagster-university/data/taxi_zones.csv"
+    )
+
+    with open(TAXI_ZONES_FILE_PATH, "wb") as output_file:
+        output_file.write(raw_zones.content)
