5a4ddab5cb0efb5a1ead865e5beee6bbfb39bbcc
julianasampar <juliana.s.sampar@gmail.com>
2025-03-13 19:22:09 -0300

    Add manhattan_stats asset and materialize

--- dagster_university/dagster_university/assets/metrics.py
+++ dagster_university/dagster_university/assets/metrics.py
@@ -1,3 +1,32 @@
 import dagster as dg
 
-from . import constants
+import matplotlib.pyplot as plt
+import geopandas as gpd
+
+import duckdb
+import os
+
+MANHATTAN_STATS_FILE_PATH = "data/staging/manhattan_stats.geojson"
+
+@dg.asset
+def manhattan_stats():
+    query = """
+        select
+            zones.zone,
+            zones.borough,
+            zones.geometry,
+            count(1) as num_trips,
+        from trips
+        left join zones on trips.pickup_zone_id = zones.zone_id
+        where borough = 'Manhattan' and geometry is not null
+        group by zone, borough, geometry
+    """
+
+    conn = duckdb.connect(os.getenv("DUCKDB_DATABASE"))
+    trips_by_zone = conn.execute(query).fetch_df()
+
+    trips_by_zone["geometry"] = gpd.GeoSeries.from_wkt(trips_by_zone["geometry"])
+    trips_by_zone = gpd.GeoDataFrame(trips_by_zone)
+
+    with open(MANHATTAN_STATS_FILE_PATH, 'w') as output_file:
+        output_file.write(trips_by_zone.to_json())
