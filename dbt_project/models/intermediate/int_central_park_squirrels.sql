b6359656a700d8c778858f320eaa04ae084acf3c
julianasampar <juliana.s.sampar@gmail.com>
2025-08-25 20:07:03 -0300

    Add transformations in stg models

--- dbt_project/models/intermediate/int_central_park_squirrels.sql
+++ dbt_project/models/intermediate/int_central_park_squirrels.sql
@@ -1,5 +1,16 @@
 {{ config(materialized='table') }}
 
-SELECT 
-   *
-FROM {{ ref('stg__nyc_central_park_squirrels_census__2018') }}
+/*SELECT 
+   squirrels.latitude,
+   trees.latitude,
+   squirrels.longitude,
+   trees.longitude
+FROM {{ ref('stg__nyc_central_park_squirrels_census__2018') }} squirrels
+INNER JOIN {{ ref('stg__nyc_street_tree_census__2015') }} trees ON 
+   ROUND(squirrels.latitude, 3) = ROUND(trees.latitude, 3)
+   AND ROUND(squirrels.longitude, 3) = ROUND(trees.longitude, 3)*/
+
+   SELECT
+      DISTINCT neighbourhood
+   FROM {{ ref('stg__nyc_street_tree_census__2015') }} trees
+;
--- dbt_project/models/staging/stg__nyc_central_park_squirrels_census__2018.sql
+++ dbt_project/models/staging/stg__nyc_central_park_squirrels_census__2018.sql
@@ -2,23 +2,23 @@
 
 SELECT 
     "Unique Squirrel Id" AS unique_squirrel_id,
-    "X" AS latitude,
-    "Y" AS longitude,
+    ROUND("X". 4) AS latitude,
+    ROUND("Y", 4) AS longitude,
     "Hectare" AS hectare,
-    "Shift" AS shift,
+    LOWER("Shift") AS shift,
     CAST(
         SUBSTR(CAST("Date" AS STRING), -4) 
             || SUBSTR(CAST("Date" AS STRING), 0, 5)
     AS INT) AS observation_date,
     "Hectare Squirrel Number" AS hectare_squirrel_number,
-    "Age" AS squirrel_age,
-    "Primary Fur Color" AS primary_fur_color,
-    "Highlight Fur Color" AS highligh_fur_color,
-    "Combination of Primary and Highlight Color" AS combination_of_primary_and_highlight_color,
-    "Color notes" AS color_notes,
-    "Location" AS location,
+    LOWER("Age") AS squirrel_age,
+    LOWER("Primary Fur Color") AS primary_fur_color,
+    LOWER("Highlight Fur Color") AS highligh_fur_color,
+    LOWER("Combination of Primary and Highlight Color") AS combination_of_primary_and_highlight_color,
+    LOWER("Color notes") AS color_notes,
+    LOWER("Location") AS location,
     "Above Ground Sighter Measurement" AS above_ground_sighter_measurement,
-    "Specific Location" AS specific_location,
+    LOWER("Specific Location") AS specific_location,
     "Running" AS was_running,
     "Chasing" AS was_chasing,
     "Climbing" AS was_climbing,
@@ -28,11 +28,11 @@ SELECT
     "Kuks" AS kuks,
     "Quaas" AS quaas,
     "Moans" AS moans,
-    "Tall flags" AS tall_flags,
-    "Tall twitches" AS tall_twitches,
+    LOWER("Tall flags") AS tall_flags,
+    LOWER("Tall twitches") AS tall_twitches,
     "Approaches" AS was_approaching,
     "Indifferent" AS was_indifferent,
-    "Runds from" AS was_running_from,
-    "Other Interactions" AS other_interactions,
-    "Lat/Long" AS latitude_longitude
+    "Runs from" AS was_running_from,
+    LOWER("Other Interactions") AS other_interactions,
+    CURRENT_TIMESTAMP AS updated_at
 FROM nyc_central_park_squirrels_census__2018
