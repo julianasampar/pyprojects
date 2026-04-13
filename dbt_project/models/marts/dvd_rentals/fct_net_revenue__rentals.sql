219dd64507c76b68db1419f3330d45c516767d13
julianasampar <juliana.s.sampar@gmail.com>
2026-04-12 23:27:13 -0300

    include expected revenue amount in calculations

--- dbt_project/models/marts/dvd_rentals/fct_net_revenue__rentals.sql
+++ dbt_project/models/marts/dvd_rentals/fct_net_revenue__rentals.sql
@@ -1,34 +1,44 @@
 -- This solution is written for SQLite querying
 
+/* 
+    In the data, not every rental has a corresponding payment. For these cases, I'm assigning 
+        revenue date as being the rental date.
+    In addition, for rentals RETURNED without a corresponding payment, I'm assuming that the 
+        payment was made, but not registered, and assigning the expected amount as being the revenue.
+    For rentals NOT RETURNED and without a corresponding payment, zero as revenue and as expected revenue.
+    
+    To calculate expected revenue amount we should divide the quantity of days the movie was rented 
+        by the rate duration (available in the film dataset). Then, multiply by the rental rate. 
+    That way, we have the expected price of the rental.
+    Looking at the data, we see that the rental rate is the minimum fee, so we should treat the data 
+        to consider that in the expected revenue as well. 
+*/
+
 WITH revenue AS (
-    /* 
-    In the data, not every rental has a corresponding payment. 
-        For this cases, we assign revenue date as being the rental date.
-    In addition, rentals made AND returned which don't have a corresponding payment, 
-        we assume that the payment was made, but not registered, and assign the rental rate 
-        as being the revenue.
-    For rentals NOT RETURNED and without a corresponding payment, we assign zero as revenue.
-    */
     SELECT 
         rental.rental_id,
         inventory.inventory_id,
         rental.customer_id,
         inventory.film_id,
         inventory.store_id,
-       DATE(COALESCE(payment.payment_date, rental.rental_date)) AS revenue_date,
-       COALESCE(payment.amount, 
-                CASE WHEN rental.return_date IS NOT NULL THEN film.rental_rate END,
-                0) AS revenue_amount,
-        JULIANDAY(DATE(rental.return_date)) - JULIANDAY(DATE(rental.rental_date)) AS days_booked
+        DATE(COALESCE(payment.payment_date, rental.rental_date)) AS revenue_date,
+        DATE(rental.return_date) AS return_date,
+                -- using set here cause sqlite doesn't let me reference a calculation without repeating code :( 
+                {% set days_booked = '(JULIANDAY(DATE(rental.return_date)) - JULIANDAY(DATE(rental.rental_date)))' %}
+        {{ days_booked }} AS days_booked,
+        CASE 
+            WHEN {{ days_booked }} < film.rental_duration
+                THEN film.rental_rate
+            ELSE ROUND(( {{ days_booked }} / film.rental_duration ), 2) * film.rental_rate
+        END AS expected_revenue,
+        payment.amount AS payment_amount
     FROM dvd_rental_store__rental rental
     LEFT JOIN dvd_rental_store__payment payment USING (rental_id)
     LEFT JOIN dvd_rental_store__inventory inventory USING (inventory_id)
     LEFT JOIN dvd_rental_store__film film USING (film_id)
 ),
 cost AS (
-    /* 
-    For the cost, we must get the replacement cost of rentals NOT RETURNED.
-    */
+    -- For the cost, we must get the replacement cost of rentals NOT RETURNED.
     SELECT 
         rental.rental_id,
         film.replacement_cost AS cost_amount -- Replacement cost should not be NULL
@@ -43,10 +53,14 @@ SELECT
     revenue.customer_id,
     revenue.film_id,
     revenue.store_id,
-    ROUND(revenue.revenue_amount, 2) AS revenue_amount,
+                -- using set here cause sqlite doesn't let me reference a calculation without repeating code :( 
+                {% set revenue_amount = 'ROUND(COALESCE(revenue.payment_amount, 
+                                            CASE WHEN revenue.return_date IS NOT NULL THEN revenue.expected_revenue END,
+                                            0), 2)' %}
+    {{ revenue_amount }} AS revenue_amount,
+    ROUND(COALESCE(revenue.expected_revenue, 0), 2) AS expected_revenue,
+    ROUND(COALESCE(1 - ({{ revenue_amount }} / revenue.expected_revenue), 0), 2) AS discount_rate,
     ROUND(COALESCE(cost.cost_amount, 0), 2) AS cost_amount,
-    ROUND(revenue.revenue_amount - COALESCE(cost.cost_amount, 0), 2) AS net_revenue_amount
+    ROUND({{ revenue_amount }} - COALESCE(cost.cost_amount, 0), 2) AS net_revenue_amount
 FROM revenue
 LEFT JOIN cost USING (rental_id)
-
---- Include rental_duration and rental_rate proportion into the calculation
